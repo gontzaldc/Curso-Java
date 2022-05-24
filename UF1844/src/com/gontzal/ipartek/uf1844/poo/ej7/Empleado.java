74ea244bb9985442d8b3cb72af2efab9482a42db
Nombre <ejemplo@ejemplo.com>
2022-05-24 13:32:06 +0200

    Inicio ejercicio 6 POO

---
+++ UF1844/src/com/gontzal/ipartek/uf1844/poo/ej7/Empleado.java
@@ -0,0 +1,79 @@
+package com.gontzal.ipartek.uf1844.poo.ej7;
+
+public class Empleado {
+
+	private static double pagoHorasExtra;
+	private String nif;
+	private String nombre;
+	private double sueldoBase;
+	private int horasExtra;
+	private double irpf;
+	private int hijos;
+	private boolean casado;
+	
+	public Empleado() {
+		
+	}
+	
+	public Empleado(String nif) {
+		this.nif=nif;
+	}
+
+	public String getNif() {
+		return nif;
+	}
+
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getSueldoBase() {
+		return sueldoBase;
+	}
+
+	public void setSueldoBase(double sueldoBase) {
+		this.sueldoBase = sueldoBase;
+	}
+
+	public int getHorasExtra() {
+		return horasExtra;
+	}
+
+	public void setHorasExtra(int horasExtra) {
+		this.horasExtra = horasExtra;
+	}
+
+	public double getIrpf() {
+		return irpf;
+	}
+
+	public void setIrpf(double irpf) {
+		this.irpf = irpf;
+	}
+
+	public int getHijos() {
+		return hijos;
+	}
+
+	public void setHijos(int hijos) {
+		this.hijos = hijos;
+	}
+
+	public boolean isCasado() {
+		return casado;
+	}
+
+	public void setCasado(boolean casado) {
+		this.casado = casado;
+	}
+	
+	
+}
