f9c4d3ecceef128ca92e2277fe7b313402e5a592
Nombre <ejemplo@ejemplo.com>
2022-06-21 13:27:05 +0200

    añadir roles a usuario

--- UF1845_usuarios/src/main/java/com/gontzal/dal/DaoUsuarioMemoria.java
+++ UF1845_usuarios/src/main/java/com/gontzal/dal/DaoUsuarioMemoria.java
@@ -4,14 +4,18 @@ import java.util.TreeMap;
 
 import com.gontzal.modelos.Usuario;
 
+
+
 class DaoUsuarioMemoria implements DaoUsuario {
 
 	private static final TreeMap<Long, Usuario> usuarios = new TreeMap<>();
 
 	static {
-		usuarios.put(1L, new Usuario(1L, "gontzal", "gontzal@gdasda.sad", "12345"));
-		usuarios.put(2L, new Usuario(2L, "usuario2", "usuario2@gdasda.sad", "2222222"));
-		usuarios.put(3L, new Usuario(3L, "usuario3", "usuario3@gdasda.sad", "3333333"));
+		
+		//TODO Poner roles a los usuarios estaticos
+		usuarios.put(1L, new Usuario(1L, "gontzal", "gontzal@gdasda.sad", "12345",null));
+		usuarios.put(2L, new Usuario(2L, "usuario2", "usuario2@gdasda.sad", "2222222",null));
+		usuarios.put(3L, new Usuario(3L, "usuario3", "usuario3@gdasda.sad", "3333333",null));
 	}
 
 	// SINGLETON
--- UF1845_usuarios/src/main/java/com/gontzal/modelos/Usuario.java
+++ UF1845_usuarios/src/main/java/com/gontzal/modelos/Usuario.java
@@ -4,17 +4,36 @@ import java.util.Objects;
 
 public class Usuario {
 
+	private enum Roles {ADMIN,USUARIO};
 	private Long id;
 	private String nombre;
 	private String email;
 	private String contrasena;
+	private Roles rol;
 
-	public Usuario(Long id, String nombre, String email, String contrasena) {
+	public Usuario(Long id, String nombre, String email, String contrasena, Roles rol) {
 
 		setId(id);
 		setNombre(nombre);
 		setEmail(email);
 		setcontrasena(contrasena);
+		setRol(rol);
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	public Roles getRol() {
+		return rol;
+	}
+
+	public void setRol(Roles rol) {
+		this.rol = rol;
 	}
 
 	public Usuario() {
@@ -54,7 +73,7 @@ public class Usuario {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(contrasena, email, id, nombre);
+		return Objects.hash(contrasena, email, id, nombre, rol);
 	}
 
 	@Override
@@ -67,12 +86,13 @@ public class Usuario {
 			return false;
 		Usuario other = (Usuario) obj;
 		return Objects.equals(contrasena, other.contrasena) && Objects.equals(email, other.email)
-				&& Objects.equals(id, other.id) && Objects.equals(nombre, other.nombre);
+				&& Objects.equals(id, other.id) && Objects.equals(nombre, other.nombre) && rol == other.rol;
 	}
 
 	@Override
 	public String toString() {
-		return "Usuario [id=" + id + ", nombre=" + nombre + ", email=" + email + ", contrasena=" + contrasena + "]";
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", email=" + email + ", contrasena=" + contrasena + ", rol="
+				+ rol + "]";
 	}
 
 }
