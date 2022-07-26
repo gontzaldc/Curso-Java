e84ba44a03b9f4305baa5562de5fbe8f32b42f71
Nombre <ejemplo@ejemplo.com>
2022-07-26 13:36:54 +0200

    login

---
+++ SPRING/springPrueba/src/main/java/com/gontzal/prueba/configuraciones/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.gontzal.prueba.configuraciones;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.*;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+		// AUTORIZACIÓN
+		@Bean
+	    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	        http
+	            .authorizeHttpRequests((authz) -> authz
+//					.antMatchers("/admin/**").authenticated()
+					.anyRequest().permitAll()
+	            )
+	            .formLogin()
+					.permitAll()
+					.and()
+				.logout()
+					.permitAll();
+		        return http.build();
+	    }
+
+	    // AUTENTICACIÓN
+		@Bean
+	    UserDetailsManager users(DataSource dataSource) {
+	        return new JdbcUserDetailsManager(dataSource);
+	    }
+		
+		@Bean
+		PasswordEncoder passwordEncoder() {
+		    return new BCryptPasswordEncoder();
+		}
+		
+		// https://bcrypt.online/
+		
+//		create table users(
+//			username varchar(50) not null primary key,
+//			password varchar(50) not null,
+//			enabled boolean not null
+//		);
+//	
+//		create table authorities (
+//			username varchar(50) not null,
+//			authority varchar(50) not null,
+//			constraint fk_authorities_users foreign key(username) references users(username)
+//		);
+//		create unique index ix_auth_username on authorities (username,authority);
+
+	}
+
