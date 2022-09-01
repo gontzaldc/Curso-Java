a5b025922df21a86401ab735999b06e82bd952b1
Nombre <ejemplo@ejemplo.com>
2022-09-01 12:54:44 +0200

    Spring prueba ejemplo tienda

---
+++ SPRING/tiendaapp/src/main/java/com/example/tienda/tiendaapp/TiendaappApplication.java
@@ -0,0 +1,13 @@
+package com.example.tienda.tiendaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaappApplication.class, args);
+	}
+
+}
---
+++ SPRING/tiendaapp/src/test/java/com/example/tienda/tiendaapp/TiendaappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tienda.tiendaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
