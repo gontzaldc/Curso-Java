bc8fec46518c6680e8a5ba47c50e82373b9335c6
Nombre <ejemplo@ejemplo.com>
2022-09-02 13:20:34 +0200

    Testing

---
+++ SPRING/tiendaapp/src/test/java/com/example/tienda/tiendaapp/repositorios/ProductoRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.example.tienda.tiendaapp.repositorios;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.tienda.tiendaapp.entidades.Producto;
+
+@SpringBootTest
+class ProductoRepositoryTest {
+
+	@Autowired
+	private ProductoRepositorio repo;
+	@Test
+	void insertarProducto() {
+		Producto producto = new Producto(null,"ordenador",new BigDecimal("2.30"),3);
+		
+		repo.save(producto);
+		
+		Producto recibido =repo.findById(1L).orElse(null);
+		assertNotNull(recibido);
+		assertEquals(producto,recibido);
+	}
+
+}
